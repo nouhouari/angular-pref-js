c0a7a432526c4bc10a132edee8606dbc682367f5
Nourreddine Houari <nourreddine.houari@sicpa.com>
2017-01-22 18:22:31 +0800

    v0.0.0

---
+++ service.js
@@ -0,0 +1,14 @@
+/** 
+ * Angular library for Preference microservice . 
+ **/
+
+angular.module('my-pharma-pref-js',[])
+      .service('my-pharma-pref-service',function(){
+
+    this.hello = function(){
+     console.log("Hello");	
+   }	
+
+});
+
+
